816ef7bf36c27eabb4f95f4a3680e42efb6fe5b2
senior_Ori <ori8834@gmail.com>
2023-03-08 21:07:04 +0200

    dev/http3attempts

--- Transmitter/esp32Transmitter/esp32Transmitter.ino
+++ Transmitter/esp32Transmitter/esp32Transmitter.ino
@@ -15,6 +15,10 @@ void setup() {
     WiFi.begin(ssid.c_str(), password.c_str());
     if (WiFi.waitForConnectResult() == WL_CONNECTED) {
       Serial.println("Connected to WiFi using saved credentials.");
+      Serial.print("ssid: ");
+      Serial.print(ssid.c_str());
+      Serial.print("\npassword: ");
+      Serial.println(password.c_str());
       getTime();
       return;
     }
@@ -47,12 +51,20 @@ void getTime() {
    HTTPClient http;
    http.begin("http://worldtimeapi.org/api/ip");
    int httpCode = http.GET();
+   int retries = 0;
+
+   while (httpCode <= 0 && retries < 3) {
+     Serial.println("Error getting time. Retrying...");
+     delay(1000);
+     httpCode = http.GET();
+     retries++;
+   }
 
    if (httpCode >0) {
      String payload = http.getString();
      Serial.println(payload);
    } else {
-     Serial.println("Error getting time");
+     Serial.println("Error getting time after 3 attempts.");
    }
 
    http.end();
